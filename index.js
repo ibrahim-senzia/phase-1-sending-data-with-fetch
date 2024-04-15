ed3a282e065c08ca53b0ce1dbf6b32f5bc32b608
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-04-15 15:30:12 +0300

    sending-data

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => {
+        return response.json();
+      })
+      .then(object => {
+        document.body.innerHTML = object.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
